32915590cc15ffb4a28e787daa81d22883b1754d
Manuel Irrazabal <manuel.irrazabalp@gmail.com>
2017-08-23 23:58:49 -0700

    Views

---
+++ resources/views/includes/scripts.blade.php
@@ -0,0 +1,28 @@
+<script src="{{url('/js/jquery.js')}}" type="text/javascript"></script>
+<script src="{{url('/js/map.js')}}" type="text/javascript"></script>
+
+<script src="{{url('/libraries/bootstrap-sass/javascripts/bootstrap/collapse.js')}}" type="text/javascript"></script>
+<script src="{{url('/libraries/bootstrap-sass/javascripts/bootstrap/carousel.js')}}" type="text/javascript"></script>
+<script src="{{url('/libraries/bootstrap-sass/javascripts/bootstrap/transition.js')}}" type="text/javascript"></script>
+<script src="{{url('/libraries/bootstrap-sass/javascripts/bootstrap/dropdown.js')}}" type="text/javascript"></script>
+<script src="{{url('/libraries/bootstrap-sass/javascripts/bootstrap/tooltip.js')}}" type="text/javascript"></script>
+<script src="{{url('/libraries/bootstrap-sass/javascripts/bootstrap/tab.js')}}" type="text/javascript"></script>
+<script src="{{url('/libraries/bootstrap-sass/javascripts/bootstrap/alert.js')}}" type="text/javascript"></script>
+
+<script src="{{url('/assets/libraries/colorbox/jquery.colorbox-min.js')}}" type="text/javascript"></script>
+
+<script src="{{url('/assets/libraries/flot/jquery.flot.min.js')}}" type="text/javascript"></script>
+<script src="{{url('/assets/libraries/flot/jquery.flot.spline.js')}}" type="text/javascript"></script>
+
+<script src="{{url('/assets/libraries/bootstrap-select/bootstrap-select.min.js')}}" type="text/javascript"></script>
+
+<script src="http://maps.googleapis.com/maps/api/js?libraries=weather,geometry,visualization,places,drawing" type="text/javascript"></script>
+
+<script type="text/javascript" src="{{url('/libraries/jquery-google-map/infobox.js')}}"></script>
+<script type="text/javascript" src="{{url('/libraries/jquery-google-map/markerclusterer.js')}}"></script>
+<script type="text/javascript" src="{{url('/libraries/jquery-google-map/jquery-google-map.js')}}"></script>
+
+<script type="text/javascript" src="{{url('/libraries/owl.carousel/owl.carousel.js')}}"></script>
+<script type="text/javascript" src="{{url('/')}}libraries/bootstrap-fileinput/fileinput.min.js"></script>
+
+<script src="{{url('/js/superlist.js')}}" type="text/javascript"></script>
