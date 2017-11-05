2ee0c982efa9352dedc1c88f1d2ee9989be89367
Manuel Irrazabal <manuel.irrazabalp@gmail.com>
2017-11-04 23:44:23 -0700

    Creating Business detail.. still in progress

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,12 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Business;
 use Illuminate\Http\Request;
+use Session;
 
 class PostsController extends Controller
 {
+    public function __construct()
+    {
+    	//For now i will bring the categories and subcategories here. 
+    	$cat = (new Category)->getCategoriesAll();
+    	Session::put('categories', $cat);
+
+
+    }
+
     public function index($slug)
     {
-    	dd($slug);
+    	if(!isset($slug)) {
+    		 return abort(404);
+    	}
+
+    	if (Session::has('categories')) {
+            $data['categories'] = Session::get('categories');
+        }
+
+        if (Session::has('userInfo')) {
+            $data['userInfo'] = Session::get('userInfo');
+        }
+
+        $data['business'] = (new Business)->getBusinessbySlug($slug);
+    	
+    	return \View::make('frontend.business', $data);
     }
 }
--- app/Models/Business.php
+++ app/Models/Business.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\BusinessImages;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\DB;
@@ -227,18 +228,77 @@ class Business extends Model
      */
     public function lastCreatedByCategory($cat)
     {
-        return  DB::table('an_business')
-                ->leftJoin('an_subcategories', 'an_business.business_cat_id', '=', 'an_subcategories.scat_id')
-                ->leftJoin('an_categories',    'an_categories.cat_id', '=', 'an_subcategories.scat_cat_id')
-                ->leftJoin('an_business_images',  'an_business.business_id', '=', 'an_business_images.bimages_business_id')
-                ->leftJoin('an_cities',    'an_business.business_city', '=', 'an_cities.id')
-                ->where('an_subcategories.scat_cat_id', $cat)
+        try {
+            $resp = DB::table('an_business')
+                ->join('an_subcategories', 'an_business.business_cat_id', '=', 'an_subcategories.scat_id')
+                ->join('an_categories',    'an_categories.cat_id', '=', 'an_subcategories.scat_cat_id')
+                ->join('an_cities',  'an_cities.id', '=', 'an_business.business_city')
                 ->where('an_business.business_active', 1)
-                ->orderBy('an_business.created_at', 'DESC')
+                ->where('an_subcategories.scat_cat_id', $cat)
+                ->latest('an_business.created_at')
                 ->limit(10)
                 ->get();
 
+            // Adding Images
+            foreach ($resp as $key => $value) {
+                $image = BusinessImages::where('bimages_business_id', $value->business_id)->first();
+                if ($image) {
+                    $value->bimage_id       = $image->bimages_id;
+                    $value->bimages_route   = $image->bimages_route;
+                }
+            }
 
+        } catch (\Exception $e) {
+            $resp['ok'] = false;
+            $resp['error'] = $e->getMessage();
+        }
+
+        return  $resp;
+    }
+
+
+     /**
+     * Get All bussines from subcategory
+     *
+     * @param integer id
+     * @return object
+     */
+    public function getBusinessBySubcat($cat)
+    {
+        try {
+            $resp = DB::table('an_business')
+                ->join('an_cities',  'an_cities.id', '=', 'an_business.business_city')
+                ->where('an_business.business_active', 1)
+                ->where('an_business.business_cat_id', $cat)
+                ->latest('an_business.created_at')
+                ->get();
+
+            // Adding Images
+            foreach ($resp as $key => $value) {
+                $image = BusinessImages::where('bimages_business_id', $value->business_id)->first();
+                if ($image) {
+                    $value->bimage_id       = $image->bimages_id;
+                    $value->bimages_route   = $image->bimages_route;
+                }
+            }
+
+        } catch (\Exception $e) {
+            $resp['ok'] = false;
+            $resp['error'] = $e->getMessage();
+        }
+
+        return  $resp;
+    }
+
+    /**
+     * Get a business by slug
+     *
+     * @param integer id
+     * @return object
+     */
+    public function getBusinessbySlug($slug)
+    {
+        return $this->where('business_slug', $slug)->where('business_active', 1)->with('businessImages')->first();
     }
 
 }
